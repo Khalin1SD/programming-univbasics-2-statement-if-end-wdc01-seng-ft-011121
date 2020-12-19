a578e3be3ceeb3aaa6900dbdbf9e0a496eb4130c
calm-collection-3916 <khalinredd@gmail.com>
2020-12-19 01:03:38 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,15 +1,4 @@
-#this is the if...end statement solution.
-my_day_was_good = true
-if my_day_was_good
-  puts "good to hear"
-end
-#this is the if...else...end statement solution.
-test_grade = 94
-if test_grade > 95
-  puts "your studies are complete"
-else
-  puts "keep on studying"
-end
+
 #this is for the if...elsif...else...end statement solution
 my_test_score = 94
 if my_test_score <= 80
