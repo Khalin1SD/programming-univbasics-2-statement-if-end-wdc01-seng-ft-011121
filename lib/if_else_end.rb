d11d76c9dd3937f48c807b0d6756e9e8a9d692eb
calm-collection-3916 <khalinredd@gmail.com>
2020-12-19 01:27:48 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,5 @@
-#this is for the if...else...end statement solution
-my_test_score = 94
-if my_test_score <= 80
-  puts "odd"
-else
-  puts "even"
-end
+#this is for the if...else...end statement solution.
+Time.now
+current_time = Time.now
+current_time.to_i
+time_as_an_integer = current_time.to_i
