431a1aa5a51b0f1052b3b261f54c97050e40cd77
calm-collection-3916 <khalinredd@gmail.com>
2020-12-19 00:44:05 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -11,3 +11,9 @@ else
   puts "keep on studying"
 end
 #this is for the if...elsif...else...end statement solution
+my_test_score = 94
+if my_test_score <= 80
+  puts "keep studying"
+  elsif (my_test_score > 81 && my_test_score < 100)
+  puts "you are done studying"
+  
