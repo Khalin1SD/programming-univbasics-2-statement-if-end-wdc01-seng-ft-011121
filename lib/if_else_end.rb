4e6696c309198b407413b53555c8913420a41fd0
calm-collection-3916 <khalinredd@gmail.com>
2020-12-19 01:05:46 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,7 @@
 #this is for the if...elsif...else...end statement solution
 my_test_score = 94
 if my_test_score <= 80
-  puts "keep studying"
-  elsif (my_test_score > 81 && my_test_score < 100)
-  puts "you are done studying"
+  puts "odd"
 else
-  puts "find a better study method"
+  puts "even"
 end
